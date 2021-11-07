1d246ce54138d8820a8e92f3d8fdd00e728813c2
Aleksander Lebedev <77786103+AleksLeb@users.noreply.github.com>
2021-11-07 22:12:09 +0300

    Update apiConfig.js

--- web-movie-with-api/src/api/apiConfig.js
+++ web-movie-with-api/src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'a11c1edc9aab60a95253db2b631ef706',
+    apiKey: 'register in themoviedb.org',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`, 
     
